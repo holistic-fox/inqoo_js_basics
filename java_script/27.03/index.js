8ab3b66cc9afbcf5176450e056e4d0bdf48fc7fb
Michał Witek <michal.witek.katowice@gmail.com>
2022-03-26 13:01:02 +0100

    basic js html manipulation

--- java_script/27.03/index.js
+++ java_script/27.03/index.js
@@ -2,23 +2,24 @@ console.log('Hello')
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    // BASIC DOM DOCUMENTATION
+    // BASIC DOM MANIPULATION
     // https://developer.mozilla.org/en-US/docs/Web/API/Event
 
     // console.log('on DOMContentLoaded', event)
     // console.log(document.querySelectorAll(".container"));
     // console.log(document.querySelector("#people-container"))
 
-    const peopleContainer = document.querySelector("#people-container");
-    peopleContainer.classList.add('people-wrapper');
-    peopleContainer.setAttribute('customAttribute', 'customValue');
-
-    const paragraph = document.createElement('p');
-    paragraph.textContent = 'this is people paragraph';
-    peopleContainer.append(paragraph);
-    peopleContainer.style.cssText = 'border: 1px solid black; cursor: pointer';
-
-    peopleContainer.addEventListener('click', (event) => {
-        console.log('click', event)
-    });
+    // const peopleContainer = document.querySelector("#people-container");
+    // peopleContainer.classList.add('people-wrapper');
+    // peopleContainer.setAttribute('customAttribute', 'customValue');
+    //
+    // const paragraph = document.createElement('p');
+    // paragraph.textContent = 'this is people paragraph';
+    // peopleContainer.append(paragraph);
+    // peopleContainer.style.cssText = 'border: 1px solid black; cursor: pointer';
+    //
+    // peopleContainer.addEventListener('click', (event) => {
+    //     console.log('click', event)
+    // });
+    // BASIC DOM MANIPULATION END
 })
